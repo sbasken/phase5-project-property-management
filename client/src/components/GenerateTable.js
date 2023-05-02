6fcefc2f1f7d3dc84363f8c2be4deaadbe024133
sbasken <120995506+sbasken@users.noreply.github.com>
2023-05-02 09:07:19 -0700

    Updated Table Contents

--- client/src/components/GenerateTable.js
+++ client/src/components/GenerateTable.js
@@ -1,73 +1,51 @@
-import { Table, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom'
+import { Table, Icon, Button } from 'semantic-ui-react';
+import { useGetExpensesQuery } from '../app/services/expensesAPI';
 
 const GenerateTable = () => {
+    const { data: expenses = [], isLoading } = useGetExpensesQuery();
+    console.log(expenses)
+
+    const tableRow = expenses.map(expense => {
+        const expenseDate = new Date(expense.date);
+        return (
+            <Table.Row>
+                <Table.Cell>{expenseDate.toLocaleDateString()}</Table.Cell>
+                <Table.Cell>{expense.expense_type}</Table.Cell>
+                <Table.Cell>{expense.amount}</Table.Cell>
+                <Table.Cell>{expense.unit_id ? expense.unit_id : 'BLDG'}</Table.Cell>
+                <Table.Cell>{expense.property_id}</Table.Cell>
+                <Table.Cell textAlign='right'>
+                    <Icon name='edit' size='large'></Icon>
+                    <Icon name='trash alternate' size='large'></Icon>
+                </Table.Cell>
+            </Table.Row>
+        );
+    });
+
   return (
     <div className='ui container hidden divider'>
+        <h1>Expenses</h1>
         <Table columns={5} unstackable>
             <Table.Header>
             <Table.Row>
                 <Table.HeaderCell>Date</Table.HeaderCell>
                 <Table.HeaderCell>Type</Table.HeaderCell>
                 <Table.HeaderCell>Amount</Table.HeaderCell>
-                <Table.HeaderCell>Unit_id</Table.HeaderCell>
+                <Table.HeaderCell>Unit ID</Table.HeaderCell>
+                <Table.HeaderCell>Property ID</Table.HeaderCell>
                 <Table.HeaderCell textAlign='right'>
-                    <Icon name='plus square outline' size='large'></Icon>
+                    <Button as={Link} to={'/expense/add-new'}>
+                       ＋
+                    </Button>
+                    
                 </Table.HeaderCell>
             </Table.Row>
             </Table.Header>
 
             <Table.Body>
-            <Table.Row>
-                <Table.Cell>4/25/2023</Table.Cell>
-                <Table.Cell>Repair</Table.Cell>
-                <Table.Cell>$280</Table.Cell>
-                <Table.Cell>A1</Table.Cell>
-                <Table.Cell textAlign='right'>
-                <Icon name='edit' size='large'></Icon>
-                <Icon name='trash alternate' size='large'></Icon>
-                </Table.Cell>
-            </Table.Row>
-            <Table.Row>
-            <Table.Cell>4/25/2023</Table.Cell>
-                <Table.Cell>Landscaping</Table.Cell>
-                <Table.Cell>$110</Table.Cell>
-                <Table.Cell>BLDG</Table.Cell>
-                <Table.Cell textAlign='right'>
-                    <Icon name='edit' size='large'></Icon>
-                    <Icon name='trash alternate' size='large'></Icon>
-                </Table.Cell>
-            </Table.Row>
-            <Table.Row>
-                <Table.Cell>4/25/2023</Table.Cell>
-                <Table.Cell>Repair</Table.Cell>
-                <Table.Cell>$280</Table.Cell>
-                <Table.Cell>A2</Table.Cell>
-                <Table.Cell textAlign='right'>
-                    <Icon name='edit' size='large'></Icon>
-                    <Icon name='trash alternate' size='large'></Icon>
-                </Table.Cell>
-            </Table.Row>
-            <Table.Row>
-                <Table.Cell>4/25/2023</Table.Cell>
-                <Table.Cell>Management Fees</Table.Cell>
-                <Table.Cell>$550</Table.Cell>
-                <Table.Cell>BLDG</Table.Cell>
-                <Table.Cell textAlign='right'>
-                    <Icon name='edit' size='large'></Icon>
-                    <Icon name='trash alternate' size='large'></Icon>
-                </Table.Cell>
-            </Table.Row>
+                {tableRow}
             </Table.Body>
-
-            <Table.Footer>
-            <Table.Row>
-                <Table.HeaderCell>3 items</Table.HeaderCell>
-                <Table.HeaderCell>2 repairs</Table.HeaderCell>
-                <Table.HeaderCell />
-                <Table.HeaderCell />
-                <Table.HeaderCell />
-            </Table.Row>
-            </Table.Footer>
         </Table>
     </div>
   )
