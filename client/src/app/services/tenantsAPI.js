f341fb58720294be3bf9aed091933636793c93db
Saki <sbasken@gmail.com>
2023-05-03 15:13:35 -0700

    Add tenantsAPI

---
+++ client/src/app/services/tenantsAPI.js
@@ -0,0 +1,61 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const tenantsAPI = createApi({
+    reducerPath: 'tenantsAPI',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:4000'
+    }),
+    tagTypes: ['Tenant'],
+    endpoints: builder => ({
+        getTenants: builder.query({
+            query() {
+                return '/tenants'
+            },
+            providesTags: ['Tenant']
+        }),
+        getTenant: builder.query({
+            query: (id) => `/tenants/${id}`
+        }),
+        addTenant: builder.mutation({
+            query: (newTenant) => ({
+                url:'/tenants',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                method: 'POST',
+                body: newTenant
+            }),
+            invalidatesTags: ['Tenant']
+        }),
+        deleteTenant: builder.mutation({
+            query: (id) => ({
+                url: `/tenants/${id}`,
+                method: 'DELETE'
+            }),
+            invalidatesTags: ['Tenant']
+        }),
+        editTenant: builder.mutation({
+            query: updatedTenant => {
+                // console.log('in editTenant', updatedTenant);
+                return {
+                  url: `/tenants/${updatedTenant.id}`,
+                  method: 'PATCH',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify(updatedTenant)
+                }
+              },
+              invalidatesTags: ['Tenant']
+        })
+
+    })
+})
+
+export const {
+     useGetuUitsQuery, 
+     useGetTenantQuery, 
+     useAddTenantMutation, 
+     useDeleteTenantMutation,
+     useEditTenantMutation
+} = tenantsAPI
--- client/src/app/store.js
+++ client/src/app/store.js
@@ -3,6 +3,7 @@ import { setupListeners } from '@reduxjs/toolkit/query'
 import { propertiesAPI } from './services/propertiesAPI'
 import { expensesAPI } from './services/expensesAPI'
 import { leasesAPI } from './services/leasesAPI'
+import { unitsAPI } from './services/unitsAPI'
 import selectedYearReducer from '../features/selectedYear/selectedYearSlice'
 
 
@@ -11,9 +12,10 @@ export const store = configureStore({
         [ propertiesAPI.reducerPath ]: propertiesAPI.reducer,
         [ expensesAPI.reducerPath ]: expensesAPI.reducer,
         [ leasesAPI.reducerPath ]: leasesAPI.reducer,
+        [ unitsAPI.reducerPath ]: unitsAPI.reducer,
         selectedYear: selectedYearReducer
     },
     middleware: getDefaultMiddleware =>
-      getDefaultMiddleware().concat(propertiesAPI.middleware, expensesAPI.middleware, leasesAPI.middleware)
+      getDefaultMiddleware().concat(propertiesAPI.middleware, expensesAPI.middleware, leasesAPI.middleware, unitsAPI.middleware)
 })
 setupListeners(store.dispatch)
