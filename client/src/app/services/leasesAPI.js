56b26a0d1c47c2bd5b1bd375e22b5a83c5a4d8b2
Saki <sbasken@gmail.com>
2023-05-03 14:11:21 -0700

    Complete setting up leasesAPI and store for leases

---
+++ client/src/app/services/leasesAPI.js
@@ -0,0 +1,61 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const leasesAPI = createApi({
+    reducerPath: 'leasesAPI',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:4000'
+    }),
+    tagTypes: ['Lease'],
+    endpoints: builder => ({
+        getLeases: builder.query({
+            query() {
+                return '/leases'
+            },
+            providesTags: ['Lease']
+        }),
+        getLease: builder.query({
+            query: (id) => `/leases/${id}`
+        }),
+        addLease: builder.mutation({
+            query: (newLease) => ({
+                url:'/leases',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                method: 'POST',
+                body: newLease
+            }),
+            invalidatesTags: ['Lease']
+        }),
+        deleteLease: builder.mutation({
+            query: (id) => ({
+                url: `/leases/${id}`,
+                method: 'DELETE'
+            }),
+            invalidatesTags: ['Lease']
+        }),
+        editLease: builder.mutation({
+            query: updatedLease => {
+                // console.log('in editLease', updatedLease);
+                return {
+                  url: `/leases/${updatedLease.id}`,
+                  method: 'PATCH',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify(updatedLease)
+                }
+              },
+              invalidatesTags: ['Lease']
+        })
+
+    })
+})
+
+export const {
+     useGetLeasesQuery, 
+     useGetLeaseQuery, 
+     useAddLeaseMutation, 
+     useDeleteLeaseMutation,
+     useEditLeaseMutation
+} = leasesAPI
--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { propertiesAPI } from './services/propertiesAPI'
 import { expensesAPI } from './services/expensesAPI'
+import { leasesAPI } from './services/leasesAPI'
 import selectedYearReducer from '../features/selectedYear/selectedYearSlice'
 
 
@@ -9,9 +10,10 @@ export const store = configureStore({
     reducer: {
         [ propertiesAPI.reducerPath ]: propertiesAPI.reducer,
         [ expensesAPI.reducerPath ]: expensesAPI.reducer,
+        [ leasesAPI.reducerPath ]: leasesAPI.reducer,
         selectedYear: selectedYearReducer
     },
     middleware: getDefaultMiddleware =>
-      getDefaultMiddleware().concat(propertiesAPI.middleware, expensesAPI.middleware)
+      getDefaultMiddleware().concat(propertiesAPI.middleware, expensesAPI.middleware, leasesAPI.middleware)
 })
 setupListeners(store.dispatch)
